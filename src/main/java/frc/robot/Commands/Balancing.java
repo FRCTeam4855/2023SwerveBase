9d8154c31511c001263b0617f4b83eb5dbac702e
4855 <robotics@shps.org>
2023-03-20 14:39:08 -0400

    Update Balancing.java

--- src/main/java/frc/robot/Commands/Balancing.java
+++ src/main/java/frc/robot/Commands/Balancing.java
@@ -12,6 +12,7 @@ public class Balancing extends CommandBase {
     public static boolean isBalancing;
     private AHRS gyro; // defines the gyro
     private long balanceTime = -1;
+    private double pitchAngleDegrees = gyro.getPitch();
 
     public Balancing(SwerveDriveSystem initialDriveSystem, AHRS initialGyro) {
         driveSystem = initialDriveSystem;
@@ -20,16 +21,23 @@ public class Balancing extends CommandBase {
 
     @Override
     public void initialize() {
-    isBalancing = true;
+        isBalancing = true;
     }
 
     @Override
     public void execute() {
+        // if (Math.abs(gyro.getPitch()) > 2) {
+        // driveSystem.moveManual(0, (gyro.getPitch() / -30 +
+        // (gyro.getPitch()/Math.abs(gyro.getPitch()) *-.1)), 0, 0,
+        // Wheel.SpeedSetting.PRECISE);
+        // }
+        // SmartDashboard.putBoolean("Balancing", isBalancing);
         if (Math.abs(gyro.getPitch()) > 2) {
-            driveSystem.moveManual(0, (gyro.getPitch() / -30 + (gyro.getPitch()/Math.abs(gyro.getPitch()) *-.1)), 0, 0, Wheel.SpeedSetting.PRECISE);
+            double pitchAngleRadians = pitchAngleDegrees * (Math.PI / 180.0);
+            double yAxisRate = Math.sin(pitchAngleRadians) * -1;
+            driveSystem.moveManual(0, yAxisRate, 0, 0, Wheel.SpeedSetting.PRECISE);
         }
         SmartDashboard.putBoolean("Balancing", isBalancing);
-
     }
 
     @Override
@@ -38,7 +46,8 @@ public class Balancing extends CommandBase {
             if (balanceTime < 0) {
                 balanceTime = System.currentTimeMillis();
             } else if (System.currentTimeMillis() - balanceTime > 1000) {
-                driveSystem.stop();  //if balanced, start counting. Reset if becomes unbalanced. finished if able to remain balanced for one full second. 
+                driveSystem.stop(); // if balanced, start counting. Reset if becomes unbalanced. finished if able to
+                                    // remain balanced for one full second.
                 isBalancing = false;
                 return true;
             }
@@ -47,6 +56,6 @@ public class Balancing extends CommandBase {
         }
         isBalancing = true;
         return false;
-        }
+    }
 
 }
